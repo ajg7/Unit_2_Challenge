0e788af265caa096d23ea9834a124aa5d670c38c
AJ <ajgebara@gmail.com>
2022-12-24 16:21:49 -0600

    Completed Challenge

--- Unit_2_Challenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Unit_2_Challenge/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,23 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    float timeInterval = 1f;
+    float timer = 1f;
+
+    void Start()
+    {
+        timer = 0f;
+    }
 
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime * timeInterval;
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && timer >= timeInterval)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            timer = 0f;
         }
     }
 }
